fe204eacc8f2afb1f487b57c40446c8db42842bc
Shaun Bouckaert <shaun@train-meditations.com>
2021-09-17 14:31:43 +1000

    Whitespace cleanup
    Tabs in Bash code, space indentation in python output
    Small spelling fix

--- argon1.sh
+++ argon1.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-# checks if file exists, delete's if it does then creates
+# checks if file exists, deletes if it does then creates
 # it with user write permissions
 argon_create_file() {
 	mkdir -p `dirname $1`
 	if [ -f $1 ]; then
-        rm $1
-    fi
+		rm $1
+	fi
 	touch $1
 	chmod 644 $1
 }
 
 # checks if a package is installed with dpkg
 argon_check_pkg() {
-    RESULT=$(dpkg-query -W -f='${Status}\n' "$1" 2> /dev/null | grep "installed")
+	RESULT=$(dpkg-query -W -f='${Status}\n' "$1" 2> /dev/null | grep "installed")
 
-    if [ "$RESULT" == "" ]; then
-        echo "NG"
-    else
-        echo "OK"
-    fi
+	if [ "$RESULT" == "" ]; then
+		echo "NG"
+	else
+		echo "OK"
+	fi
 }
 
 argon_get_platform() {
 	CHECKPLATFORM="Others"
-	# Check if Raspbian, otherwise Ubuntu	
+	# Check if Raspbian, otherwise Ubuntu
 	if (grep -q -F 'Raspbian' /etc/os-release &> /dev/null); then
 		CHECKPLATFORM="Raspbian"
 	fi
@@ -155,7 +155,7 @@ argon_create_shutdownscript() {
 
 	if len(sys.argv)>1:
 	    bus.write_byte(0x1a,0)
-		#powercut signal
+	    #powercut signal
 	    if sys.argv[1] == "poweroff" or sys.argv[1] == "halt":
 	        try:
 	            bus.write_byte(0x1a,0xFF)
@@ -335,37 +335,37 @@ argon_create_removescript() {
 	read -p "Press Y to continue: " -n 1 -r
 	echo
 	if ! [[ $REPLY =~ ^[yY] ]]; then
-	    echo "Cancelled"
-	    exit 1
+		echo "Cancelled"
+		exit 1
 	fi
 	if [ -d $SHORTCUT_PREFIX ]; then
-	    rm "${SHORTCUT_PREFIX}argonone-config.desktop"
-	    rm "${SHORTCUT_PREFIX}argonone-uninstall.desktop"
-	    rm "${PREFIX}/share/pixmaps/ar1config.png"
-	    rm "${PREFIX}/share/pixmaps/ar1uninstall.png"
+		rm "${SHORTCUT_PREFIX}argonone-config.desktop"
+		rm "${SHORTCUT_PREFIX}argonone-uninstall.desktop"
+		rm "${PREFIX}/share/pixmaps/ar1config.png"
+		rm "${PREFIX}/share/pixmaps/ar1uninstall.png"
 	fi
 	if [ -f $powerbuttonscript ]; then
-	    systemctl stop $daemonname.service
-	    systemctl disable $daemonname.service
-	    systemctl daemon-reload
-	    $shutdownscript uninstall
-	    rm $powerbuttonscript
-	    rm $shutdownscript
-	    rm $configscript
-	    rm $removescript
-	    rm $daemonfanservice
-	    echo "Config file remains at ${daemonconfigfile}"
-	    read -p "Press Y to delete: " -n 1 -r
+		systemctl stop $daemonname.service
+		systemctl disable $daemonname.service
+		systemctl daemon-reload
+		$shutdownscript uninstall
+		rm $powerbuttonscript
+		rm $shutdownscript
+		rm $configscript
+		rm $removescript
+		rm $daemonfanservice
+		echo "Config file remains at ${daemonconfigfile}"
+		read -p "Press Y to delete: " -n 1 -r
 		echo
 		if [[ $REPLY =~ ^[yY] ]]; then
-	        rm $daemonconfigfile
+			rm $daemonconfigfile
 		fi
-	    echo "Removed Argon One Services."
-	    if [ "${#install_list[@]}" -gt 0 ]; then
-	        echo
-	        echo "The following packages were installed by the argonone installer:"
+		echo "Removed Argon One Services."
+		if [ "${#install_list[@]}" -gt 0 ]; then
+			echo
+			echo "The following packages were installed by the argonone installer:"
 			echo "	${install_list[@]}"
-	        echo
+			echo
 			echo "If they are no longer needed you can remove them."
 		fi
 	fi
@@ -396,97 +396,97 @@ argon_create_configscript() {
 	read -p "Press Y to continue: " -n 1 -r
 	echo
 	if ! [[ $REPLY =~ ^[yY] ]]; then
-	    echo "Cancelled"
-	    exit 1
+		echo "Cancelled"
+		exit 1
 	fi
 	echo "Thank you."
 
 	get_number() {
-	    re="^[0-9]+\$" #function checks if value between 0-100, don't test sign
-	    read curnumber
-	    if [ -z "$curnumber" ]
-	    then
-	        echo "-2"
-	        return
-	    elif [[ $curnumber =~ $re ]]
-	    then
-	        if [ $curnumber -lt 0 ]
-	        then
-	            echo "-1"
-	            return
-	        elif [ $curnumber -gt 100 ]
-	        then
-	            echo "-1"
-	            return
-	        fi	
-	        echo $curnumber
-	        return
-	    fi
-	    echo "-1"
-	    return
+		re="^[0-9]+\$" #function checks if value between 0-100, don't test sign
+		read curnumber
+		if [ -z "$curnumber" ]
+		then
+			echo "-2"
+			return
+		elif [[ $curnumber =~ $re ]]
+		then
+			if [ $curnumber -lt 0 ]
+			then
+				echo "-1"
+				return
+			elif [ $curnumber -gt 100 ]
+			then
+				echo "-1"
+				return
+			fi	
+			echo $curnumber
+			return
+		fi
+		echo "-1"
+		return
 	}
 	
 
 	loopflag=1
 	while [ $loopflag -eq 1 ]
 	do
-	    echo
-	    echo "Select fan mode:"
-	    echo "  1. Always on"
-	    echo "  2. Adjust to temperatures (55C, 60C, and 65C)"
-	    echo "  3. Customize behavior"
-	    echo "  4. Cancel"
-	    echo "NOTE: You can also edit $daemonconfigfile directly"
-	    echo -n "Enter Number (1-4):"
-	    newmode=$( get_number )
-	    if [[ $newmode -ge 1 && $newmode -le 4 ]]
-	    then
-	        loopflag=0
-	    fi
+		echo
+		echo "Select fan mode:"
+		echo "  1. Always on"
+		echo "  2. Adjust to temperatures (55C, 60C, and 65C)"
+		echo "  3. Customize behavior"
+		echo "  4. Cancel"
+		echo "NOTE: You can also edit $daemonconfigfile directly"
+		echo -n "Enter Number (1-4):"
+		newmode=$( get_number )
+		if [[ $newmode -ge 1 && $newmode -le 4 ]]
+		then
+			loopflag=0
+		fi
 	done
 
 	echo
 	if [ $newmode -eq 4 ]
 	then
-	    echo "Cancelled"
-	    exit
+		echo "Cancelled"
+		exit
 	elif [ $newmode -eq 1 ]
 	then
-	    echo "#" > $daemonconfigfile
-	    echo "# Argon One Fan Speed Configuration" >> $daemonconfigfile
-	    echo "#" >> $daemonconfigfile
-	    echo "# Min Temp=Fan Speed" >> $daemonconfigfile
-	    echo 1"="100 >> $daemonconfigfile
-	    systemctl restart $daemonname.service
-	    echo "Fan always on."
-	    exit
+		echo "#" > $daemonconfigfile
+		echo "# Argon One Fan Speed Configuration" >> $daemonconfigfile
+		echo "#" >> $daemonconfigfile
+		echo "# Min Temp=Fan Speed" >> $daemonconfigfile
+		echo 1"="100 >> $daemonconfigfile
+		systemctl restart $daemonname.service
+		echo "Fan always on."
+		exit
 	elif [ $newmode -eq 2 ]
 	then
-	    echo "Please provide fan speeds for the following temperatures:"
-	    echo "#" > $daemonconfigfile
-	    echo "# Argon One Fan Speed Configuration" >> $daemonconfigfile
-	    echo "#" >> $daemonconfigfile
-	    echo "# Min Temp=Fan Speed" >> $daemonconfigfile
-	    curtemp=55
-	    while [ $curtemp -lt 70 ]
-	    do
-	        errorfanflag=1
-	        while [ $errorfanflag -eq 1 ]
-	        do
-	            echo -n ""$curtemp"C (0-100 only):"
-	            curfan=$( get_number )
-	            if [ $curfan -ge 0 ]
-	            then
-	                errorfanflag=0
-	            fi
-	        done
-	        echo $curtemp"="$curfan >> $daemonconfigfile
-	        curtemp=$((curtemp+5))
-	    done
+		echo "Please provide fan speeds for the following temperatures:"
+		echo "#" > $daemonconfigfile
+		echo "# Argon One Fan Speed Configuration" >> $daemonconfigfile
+		echo "#" >> $daemonconfigfile
+		echo "# Min Temp=Fan Speed" >> $daemonconfigfile
+		curtemp=55
+		while [ $curtemp -lt 70 ]
+		do
+			errorfanflag=1
+			while [ $errorfanflag -eq 1 ]
+			do
+				echo -n ""$curtemp"C (0-100 only):"
+				curfan=$( get_number )
+				if [ $curfan -ge 0 ]
+				then
+					errorfanflag=0
+				fi
+			done
+			echo $curtemp"="$curfan >> $daemonconfigfile
+			curtemp=$((curtemp+5))
+		done
 
-	    systemctl restart $daemonname.service
-	    echo "Configuration updated."
-	    exit
+		systemctl restart $daemonname.service
+		echo "Configuration updated."
+		exit
 	fi
 
 	echo "Please provide fan speeds and temperature pairs"
@@ -496,62 +496,62 @@ argon_create_configscript() {
 	paircounter=0
 	while [ $loopflag -eq 1 ]
 	do
-	    errortempflag=1
-	    errorfanflag=1
-	    while [ $errortempflag -eq 1 ]
-	    do
-	        echo -n "Provide minimum temperature (in Celsius) then [ENTER]:"
-	        curtemp=$( get_number )
-	        if [ $curtemp -ge 0 ]
-	        then
-	            errortempflag=0
-	        elif [ $curtemp -eq -2 ]
-	        then
-	            errortempflag=0
-	            errorfanflag=0
-	            loopflag=0
-	        fi
-	    done
-	    while [ $errorfanflag -eq 1 ]
-	    do
-	        echo -n "Provide fan speed for "$curtemp"C (0-100) then [ENTER]:"
-	        curfan=$( get_number )
-	        if [ $curfan -ge 0 ]
-	        then
-	            errorfanflag=0
-	        elif [ $curfan -eq -2 ]
-	        then
-	            errortempflag=0
-	            errorfanflag=0
-	            loopflag=0
-	        fi
-	    done
-	    if [ $loopflag -eq 1 ]
-	    then
-	        if [ $paircounter -eq 0 ]
-	        then
-	            echo "#" > $daemonconfigfile
-	            echo "# Argon One Fan Speed Configuration" >> $daemonconfigfile
-	            echo "#" >> $daemonconfigfile
-	            echo "# Min Temp=Fan Speed" >> $daemonconfigfile
-	        fi
-	        echo $curtemp"="$curfan >> $daemonconfigfile
-	        
-	        paircounter=$((paircounter+1))
-	        
-	        echo "* Fan speed will be set to "$curfan" once temperature reaches "$curtemp" C"
-	        echo
-	    fi
+		errortempflag=1
+		errorfanflag=1
+		while [ $errortempflag -eq 1 ]
+		do
+			echo -n "Provide minimum temperature (in Celsius) then [ENTER]:"
+			curtemp=$( get_number )
+			if [ $curtemp -ge 0 ]
+			then
+				errortempflag=0
+			elif [ $curtemp -eq -2 ]
+			then
+				errortempflag=0
+				errorfanflag=0
+				loopflag=0
+			fi
+		done
+		while [ $errorfanflag -eq 1 ]
+		do
+			echo -n "Provide fan speed for "$curtemp"C (0-100) then [ENTER]:"
+			curfan=$( get_number )
+			if [ $curfan -ge 0 ]
+			then
+				errorfanflag=0
+			elif [ $curfan -eq -2 ]
+			then
+				errortempflag=0
+				errorfanflag=0
+				loopflag=0
+			fi
+		done
+		if [ $loopflag -eq 1 ]
+		then
+			if [ $paircounter -eq 0 ]
+			then
+				echo "#" > $daemonconfigfile
+				echo "# Argon One Fan Speed Configuration" >> $daemonconfigfile
+				echo "#" >> $daemonconfigfile
+				echo "# Min Temp=Fan Speed" >> $daemonconfigfile
+			fi
+			echo $curtemp"="$curfan >> $daemonconfigfile
+			
+			paircounter=$((paircounter+1))
+			
+			echo "* Fan speed will be set to "$curfan" once temperature reaches "$curtemp" C"
+			echo
+		fi
 	done
 	
 	echo
 	if [ $paircounter -gt 0 ]
 	then
-	    echo "Thank you!  We saved "$paircounter" pairs."
-	    systemctl restart $daemonname.service
-	    echo "Changes should take effect now."
+		echo "Thank you!  We saved "$paircounter" pairs."
+		systemctl restart $daemonname.service
+		echo "Changes should take effect now."
 	else
-	    echo "Cancelled, no data saved."
+		echo "Cancelled, no data saved."
 	fi
 	EOF
 
