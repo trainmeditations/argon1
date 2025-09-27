7a7844c7adfd52c7046402a610a0fa832ae31524
Shaun Bouckaert <shaun@train-meditations.com>
2025-09-27 16:15:34 +1000

    Renamed script files to be .sh type

--- checkoriginalupdates.sh
+++ checkoriginalupdates.sh
@@ -1,9 +1,9 @@
 #/usr/bin/env bash
-wget -O argon1.sh.wget https://download.argon40.com/argon1.sh
-lines=$(sdiff -sd argon1.sh.original argon1.sh.wget | wc -l)
+wget -O argon1.wget.sh https://download.argon40.com/argon1.sh
+lines=$(sdiff -sd argon1.original.sh argon1.wget.sh | wc -l)
 if [ $lines -eq 0 ]; then
     echo No Update
-    rm argon1.sh.wget
+    rm argon1.wget.sh
 else
-    echo Source Updated! Copy from argon1.sh.wget for new script
+    echo Source Updated! Copy from argon1.wget.sh for new script
 fi
