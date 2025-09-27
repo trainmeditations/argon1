ac2489cb4abb4d653f5594d92eb1ab9cb1342b32
Shaun Bouckaert <shaun@train-meditations.com>
2025-09-27 16:23:15 +1000

    Fixed update check shebang

--- checkoriginalupdates.sh
+++ checkoriginalupdates.sh
@@ -1,4 +1,4 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 wget -O argon1.wget.sh https://download.argon40.com/argon1.sh
 lines=$(sdiff -sd argon1.original.sh argon1.wget.sh | wc -l)
 if [ $lines -eq 0 ]; then
